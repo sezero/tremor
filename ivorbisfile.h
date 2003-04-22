46ec6d77c8a88b45e003eb343522d5a0e9a86705
Monty <xiphmont@xiph.org>
2003-04-22 09:21:35 +0000

    copyrights :-)
    git-svn-id: https://svn.xiph.org/branches/lowmem-branch/Tremor@4626 0101bb08-14d6-0310-b084-bc0e0c8e3800

--- ivorbisfile.h
+++ ivorbisfile.h
@@ -6,7 +6,7 @@
  * GOVERNED BY A BSD-STYLE SOURCE LICENSE INCLUDED WITH THIS SOURCE *
  * IN 'COPYING'. PLEASE READ THESE TERMS BEFORE DISTRIBUTING.       *
  *                                                                  *
- * THE OggVorbis 'TREMOR' SOURCE CODE IS (C) COPYRIGHT 1994-2002    *
+ * THE OggVorbis 'TREMOR' SOURCE CODE IS (C) COPYRIGHT 1994-2003    *
  * BY THE Xiph.Org FOUNDATION http://www.xiph.org/                  *
  *                                                                  *
  ********************************************************************
@@ -27,6 +27,7 @@ extern "C"
 #include "ivorbiscodec.h"
 
 #define CHUNKSIZE 1024
+
 /* The function prototypes for the callbacks are basically the same as for
  * the stdio functions fread, fseek, fclose, ftell. 
  * The one difference is that the FILE * arguments have been replaced with
