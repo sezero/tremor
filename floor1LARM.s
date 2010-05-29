d4a99079c96f680aae57af6a49a4c0c82eea0308
Robin Watts <robin@xiph.org>
2010-05-29 12:52:40 +0000

    Move back to ARM format assembler files, but add a script to convert from arm2gnu format. Update revised temporary makefile that now invokes the conversion script as required.
    git-svn-id: https://svn.xiph.org/branches/lowmem-branch/Tremolo@17254 0101bb08-14d6-0310-b084-bc0e0c8e3800

--- floor1LARM.s
+++ floor1LARM.s
@@ -1,35 +1,35 @@
-@ Tremolo library
-@ Copyright (C) 2009 Robin Watts for Pinknoise Productions Ltd
+; Tremolo library
+; Copyright (C) 2009 Robin Watts for Pinknoise Productions Ltd
 
-    .text
+	AREA	|.text|, CODE, READONLY
 
-	.global	render_line_arm_low
+	EXPORT	render_line_arm_low
 
-render_line_arm_low:
-	@ r0 = n
-	@ r1 = d
-	@ r2 = floor
-	@ r3 = base
-	@ <> = err
-	@ <> = adx
-	@ <> = ady
+render_line_arm_low
+	; r0 = n
+	; r1 = d
+	; r2 = floor
+	; r3 = base
+	; <> = err
+	; <> = adx
+	; <> = ady
 	MOV	r12,r13
 	STMFD	r13!,{r4-r6,r11,r14}
-	LDMFD	r12,{r11,r12,r14}	@ r11 = err
-					@ r12 = adx
-					@ r14 = ady
-rl_loop:
-	LDR	r4, [r1]		@ r4 = *d
-	LDR	r5, [r2], r3,LSL #2	@ r5 = *floor    r2 = floor+base
-	SUBS	r11,r11,r14		@ err -= ady
+	LDMFD	r12,{r11,r12,r14}	; r11 = err
+					; r12 = adx
+					; r14 = ady
+rl_loop
+	LDR	r4, [r1]		; r4 = *d
+	LDR	r5, [r2], r3,LSL #2	; r5 = *floor    r2 = floor+base
+	SUBS	r11,r11,r14		; err -= ady
 	MOV	r4, r4, ASR #6
-	MUL	r5, r4, r5		@ r5 = MULT31_SHIFT15
-	ADDLT	r11,r11,r12		@ if (err < 0) err+=adx
-	ADDLT	r2, r2, #4		@              floor+=1
+	MUL	r5, r4, r5		; r5 = MULT31_SHIFT15
+	ADDLT	r11,r11,r12		; if (err < 0) err+=adx
+	ADDLT	r2, r2, #4		;              floor+=1
 	SUBS	r0, r0, #1
 	STR	r5, [r1], #4
 	BGT	rl_loop
 
 	LDMFD	r13!,{r4-r6,r11,PC}
 
-	@ END
+	END
