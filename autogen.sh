820fb3237ea81af44c9cc468c8b4e20128e3e5ad
Petter Reinholdtsen <pere@debian.org>
2025-04-02 15:43:29 +0200

    Updated autotools setup to avoid warnings from autogen.sh.
    Partly based on running 'autoupdate', and partly manual editing
    to fix warnings.
    
    Also drop running configure at the end of autogen.sh, to behave
    more like other packages and remove the need for 'make distclean'
    in CI.
    
    Fixes #1922

--- autogen.sh
+++ autogen.sh
@@ -98,11 +98,6 @@ if test "$DIE" -eq 1; then
         exit 1
 fi
 
-if test -z "$*"; then
-        echo "I am going to run ./configure with no arguments - if you wish "
-        echo "to pass any to it, please specify them on the $0 command line."
-fi
-
 echo "Generating configuration files for $package, please wait...."
 
 echo "  $ACLOCAL $ACLOCAL_FLAGS"
@@ -115,6 +110,3 @@ echo "  $AUTOMAKE --add-missing $AUTOMAKE_FLAGS"
 $AUTOMAKE --add-missing $AUTOMAKE_FLAGS || exit 1
 echo "  autoconf"
 autoconf || exit 1
-
-cd $olddir
-$srcdir/configure --enable-maintainer-mode "$@" && echo
